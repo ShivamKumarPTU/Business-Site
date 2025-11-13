2affd5e53a56cdd2711c40b559bd54bdfa88e9a6
Shivam Kumar <145775372+ShivamKumarPTU@users.noreply.github.com>
2025-11-14 01:39:55 +0530

    Index.html file Update

--- h.py
+++
@@ -1,9 +0,0 @@
-def count_word(string, word):
-    words = string.split()  # Split the input string into a list of individual words based on spaces
-    return words.count(word)  # Return the number of occurrences of 'word' in the list
-
-# Example usage:
-sentence = "This is an example sentence that contains multiple instances of some repeating words"
-target_word = "example"
-occurrences = count_word(sentence, target_word)
-print("The word", target_word, "appears", occurrences, "time in the string.")
